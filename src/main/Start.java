842a3e9fb6666044ad157cb85a6a353eeb4ac143
lahouaia <lahouaia@if08.iut-infobio.priv.univ-lille1.fr>
2020-09-02 11:14:18 +0200

    Resolution de bug

--- src/main/Start.java
+++ src/main/Start.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 
 import main.Plateau;
 
-<<<<<<< HEAD
 public class Start {
 
 	static Coordinate spawnT1 ;
@@ -172,172 +171,7 @@ public class Start {
 		System.out.println(" 1-Deplacement \n 2-Action ");
 		String answer=scan.next();
 
-		
-		
-		
-=======
-public class start {
 
-	static Coordinate spawnT1 ;
-	static Coordinate spawnT2 ;
-	
-	//pour la taille, prendre un nombre divisible par 3
-	static final int TAILLE_MAP = 9 ;
-	
-	public static void main(String[] args) throws IOException {
-		
-		menu();
-
-	}
-	
-	public static void menu() throws IOException {
-		clear();
-		System.out.println("     ~~ Donjon Baston ~~\n\n\nVeuillez choisir une option du menu : \n 1 - Start \n 2 - Regles \n 3 - Credits");
-		Scanner scan = new Scanner(System.in);
-		String answer = scan.next();
-		switch (answer) {
-		case "1":
-			partie();
-			break;
-			
-		case "2":
-			regles();
-			break;
-			
-		case "3":
-			credits();
-			break;
-
-		default:
-			break;
-		}
-	}
-	
-	
-	public static void regles() throws IOException {
-		clear();
-		BufferedReader lecteur = null;
-		String regles;
-		try {
-			lecteur=new BufferedReader(new FileReader("src/main/ReglesDuJeuDonjonBaston.txt"));
-		}catch (FileNotFoundException e) {
-			System.out.println("Erreur d'ouverture ...");
-		}while ((regles=lecteur.readLine())!=null) {
-			System.out.println(regles);
-			
-		}
-		lecteur.close();
-		System.out.println("\n\n\n Appuyez sur M pour retourner au Menu !");
-		Scanner scan = new Scanner(System.in);
-		String answer = scan.next();
-		if (answer.equalsIgnoreCase("m")) {
-			menu();
-		}else{
-			regles();
-		}
-	}
-	
-	public static void credits() throws IOException {
-		clear();
-		System.out.println("Cr�dits : \n Antoine DELABY \n Charles DESBIENS \n Ayoub LAHOUAICHRI \n Corentin LEBLEU \n Maxime SOUDANT \n\n\n Appuyez sur M pour retourner au Menu !");
-		Scanner scan = new Scanner(System.in);
-		String answer = scan.next();
-		if (answer.equalsIgnoreCase("m")) {
-			menu();
-		}else{
-			credits();
-		}
-	}
-	
-	public static void clear() {
-		for (int i=0; i<50; i++) {
-			System.out.println("\n");
-		}
-	}
-	
-	public static void partie() throws IOException {
-		Random rand = new Random();
-		
-		Team team1 = new Team();
-		Team team2 = new Team();
-		creationDesTeam(team1);
-		System.out.println("\n\n\n");
-		creationDesTeam(team2);
-		
-		Plateau map = new Plateau(TAILLE_MAP,TAILLE_MAP);
-		map.setTeam(team1, 0, rand.nextInt(TAILLE_MAP));
-		map.setTeam(team2, TAILLE_MAP-1, rand.nextInt(TAILLE_MAP));
-		spawnT1 = team1.getCoordinate() ;
-		spawnT2 = team2.getCoordinate() ;
-		
-		MonsterMap(map);
-		
-		deroulement(map, team1, team2);
-		
-	}
-	
-	private static int verification(Team team1, Team team2) {
-		if(team1.getHealthPoint() <= 0) return 2;
-		if(team2.getHealthPoint() <= 0) return 1;
-		if(team1.getCoordinate().equals(spawnT1) && team1.isTresorRecupere()) return 1 ;
-		if(team2.getCoordinate().equals(spawnT2) && team2.isTresorRecupere()) return 2 ;
-		return 0;
-	}
-	public static void deroulement(Plateau map, Team team1, Team team2) throws IOException {
-		clear();
-		affichage(map, team1, team2);
-		int vainqueur = 0 ;
-		while(vainqueur == 0) {
-			play(map, team1);
-			vainqueur = verification(team1,team2) ;
-			clear();
-			affichage(map, team1, team2);
-			if (vainqueur != 0)	break; 
-			play(map, team2);
-			clear();
-			affichage(map, team1, team2);
-			vainqueur = verification(team1,team2) ;
-		}
-		clear();
-		affichage(map, team1, team2);
-		messageDeFin(vainqueur) ;
-
-	}
-	
-	private static void messageDeFin(int vainqueur) throws IOException {
-		switch (vainqueur) {
-		case 1:
-			clear();
-			System.out.println("Le vainqueur est le joueur 1.\n Felicitation!!!");
-			try {
-				Thread.sleep(5000);
-				credits();
-			}catch (InterruptedException ie) {
-				
-			}
-			break;
-		case 2:
-			clear();
-			System.out.println("Le vainqueur est le joueur 2.\n Felicitation!!!");
-			try {
-				Thread.sleep(5000);
-				credits();
-			}catch (InterruptedException ie) {
-				
-			}
-			break;
-		}
-		
-	}
-
-
-	public static void play(Plateau map, Team team) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("\n\n\nTeam "+team.toString()+" Choisissez votre action :\n");
-		System.out.println(" 1-Deplacement \n 2-Action ");
-		String answer=scan.next();
-
->>>>>>> branch 'master' of https://gitlab.univ-lille.fr/2020-S3-projet/groupe-5.git
 		while (Integer.parseInt(answer)<1||Integer.parseInt(answer)>2) {
 			System.out.println("Choix non disponible, choisis parmis : \n 1-Deplacement \n 2-Action ");
 			answer = scan.next();
@@ -488,7 +322,4 @@ public class start {
 		}
 			
 	}
-	
-	
-
 }
